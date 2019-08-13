9b7eb336f61110c1444ec6a056214feb33961954
Edmar Alves <47756547+EdmarAlvez@users.noreply.github.com>
2019-08-13 14:27:46 -0300

    Apreendendo PHP
    Curso de dev WEB da udemy PHP

---
+++ mensagens_divertidas.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<title>Mensagens divertidas</title>
+	</head>
+
+	<body>
+
+		<?php 
+
+			//int
+			$valor_inteiro = 1 * 4;
+			echo $valor_inteiro;
+			echo "<br>";
+
+			$valor_inteiro = $valor_inteiro *2;
+			echo $valor_inteiro;
+
+			echo "<br>";
+
+			//float
+			$valor_fracionado = 10.1;
+			echo $valor_fracionado;
+
+			echo '<br>';
+
+			//bollean
+			$estado = true; #false
+			echo $estado;
+
+			echo "<br>";
+
+			//string
+			$texto = 'Curso de PhP - ' .$valor_inteiro;
+			echo $texto;
+
+			echo "<br>";
+			
+			//arrays
+
+
+		?>
+
+	
+
+	</body>
+
+</html>
