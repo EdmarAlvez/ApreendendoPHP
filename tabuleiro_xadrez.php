d2c4b8f31cbe8afcd1c49a824c64ec9a41f4c098
Edmar Alves <47756547+EdmarAlvez@users.noreply.github.com>
2019-08-13 15:31:53 -0300

    tabuleiro_xadrez.php
    APREENDENDO PHP

---
+++ tabuleiro_xadrez.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<title>Tabuleiro de xadrez</title>
+	</head>
+
+	<body>
+
+		<?php 
+
+			
+			$tabuleiro[8]['a'] = 'torre preta';
+			$tabuleiro[8]['b'] = 'bispo preto';	
+			$tabuleiro[8]['c'] = 'cavalo preto';
+			$tabuleiro[8]['d'] = 'rainha preta';
+			$tabuleiro[8]['e'] = 'rei preto';
+			$tabuleiro[8]['f'] = 'bispo preto';
+			$tabuleiro[8]['g'] = 'cavalo preto';
+			$tabuleiro[8]['h'] = 'torre preto';
+
+			$tabuleiro[7]['a'] = 'peão preto';
+			$tabuleiro[7]['b'] = 'peão preto';
+			$tabuleiro[7]['c'] = 'peão preto';
+			$tabuleiro[7]['d'] = 'peão preto';
+			$tabuleiro[7]['e'] = 'peão preto';
+			$tabuleiro[7]['f'] = 'peão preto';
+			$tabuleiro[7]['g'] = 'peão preto';
+			$tabuleiro[7]['h'] = 'peão preto';
+
+/*			$tabuleiro[6]['a'] = 'bispo preto';
+			$tabuleiro[6]['b'] = 'bispo preto';
+			$tabuleiro[6]['c'] = 'bispo preto';
+			$tabuleiro[6]['d'] = 'bispo preto';
+			$tabuleiro[6]['e'] = 'bispo preto';
+			$tabuleiro[6]['f'] = 'bispo preto';
+			$tabuleiro[6]['g'] = 'bispo preto';
+			$tabuleiro[6]['h'] = 'bispo preto';
+
+			$tabuleiro[5]['a'] = 'bispo preto';
+			$tabuleiro[5]['b'] = 'bispo preto';
+			$tabuleiro[5]['c'] = 'bispo preto';
+			$tabuleiro[5]['d'] = 'bispo preto';
+			$tabuleiro[5]['e'] = 'bispo preto';
+			$tabuleiro[5]['f'] = 'bispo preto';
+			$tabuleiro[5]['g'] = 'bispo preto';
+			$tabuleiro[5]['h'] = 'bispo preto';
+
+			$tabuleiro[4]['a'] = 'bispo preto';
+			$tabuleiro[4]['b'] = 'bispo preto';
+			$tabuleiro[4]['c'] = 'bispo preto';
+			$tabuleiro[4]['d'] = 'bispo preto';
+			$tabuleiro[4]['e'] = 'bispo preto';
+			$tabuleiro[4]['f'] = 'bispo preto';
+			$tabuleiro[4]['g'] = 'bispo preto';
+			$tabuleiro[4]['h'] = 'bispo preto';
+
+			$tabuleiro[3]['a'] = 'bispo preto';
+			$tabuleiro[3]['b'] = 'bispo preto';
+			$tabuleiro[3]['c'] = 'bispo preto';
+			$tabuleiro[3]['d'] = 'bispo preto';
+			$tabuleiro[3]['e'] = 'bispo preto';
+			$tabuleiro[3]['f'] = 'bispo preto';
+			$tabuleiro[3]['g'] = 'bispo preto';
+			$tabuleiro[3]['h'] = 'bispo preto';
+
+			$tabuleiro[2]['a'] = 'bispo preto';
+			$tabuleiro[2]['b'] = 'bispo preto';
+			$tabuleiro[2]['c'] = 'bispo preto';
+			$tabuleiro[2]['d'] = 'bispo preto';
+			$tabuleiro[2]['e'] = 'bispo preto';
+			$tabuleiro[2]['f'] = 'bispo preto';
+			$tabuleiro[2]['g'] = 'bispo preto';
+			$tabuleiro[2]['h'] = 'bispo preto';
+
+			$tabuleiro[1]['a'] = 'bispo preto';
+			$tabuleiro[1]['b'] = 'bispo preto';
+			$tabuleiro[1]['c'] = 'bispo preto';
+			$tabuleiro[1]['d'] = 'bispo preto';
+			$tabuleiro[1]['e'] = 'bispo preto';
+			$tabuleiro[1]['f'] = 'bispo preto';
+			$tabuleiro[1]['g'] = 'bispo preto';
+			$tabuleiro[1]['h'] = 'bispo preto'; */
+
+
+
+			print_r($tabuleiro);
+			print '<br>';
+			print $tabuleiro[8]['c'];
+
+
+
+
+		?>
+
+	
+
+	</body>
+
+</html>
